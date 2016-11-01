fc8d1c2ce3d9a2ca2e5eca7c4a39bb40a915e56c
vipinvpn <v.vipin43@gmail.com>
2016-11-01 22:21:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one', function (req, res) {
+    res.sent('Article one requested');
+});
+
+app.get('article-two', function (req, res) {
+    res.sent('Article two requested');
+});
+
+app.get('article-three', function (req, res) {
+    res.sent('Article three requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
